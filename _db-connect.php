245649efd161e9f24b0103bb8e7ea5d6fc2994ca
Allan Im <allan@vogle.com>
2018-10-23 17:20:47 -0400

    Add db connector

---
+++ _db-connect.php
@@ -0,0 +1,8 @@
+<?php
+$database = "work4u";
+$db_host = "localhost";
+$db_user = "root";
+$db_password = "root";
+$db_port = "8889";
+
+$connection = mysqli_connect($db_host, $db_user, $db_password, $database, $db_port);
