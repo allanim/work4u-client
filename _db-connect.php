98ac6a40a292fd6884e49bfea830c9124786195a
Allan Im <allan@vogle.com>
2018-11-15 17:38:27 -0500

    Update db info

--- _db-connect.php
+++ _db-connect.php
@@ -1,8 +1,8 @@
 <?php
+$db_host = "work4u.mysql.database.azure.com";
+$db_user = "work4u@work4u";
+$db_password = "work4u!!";
 $database = "work4u";
-$db_host = "localhost";
-$db_user = "root";
-$db_password = "root";
-$db_port = "8889";
+$db_port = "3306";
 
 $connection = mysqli_connect($db_host, $db_user, $db_password, $database, $db_port);
